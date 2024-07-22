62eec35dd2969e4a9e8ec508f91e2cd9f8afe51b
saurabh-singh-9090 <90360saurabh@gmail.com>
2024-07-23 02:29:12 +0530

    added mongodb atlas uri

--- crypto_tracker_backend/config/db.js
+++ crypto_tracker_backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/cryptoDB';
+// const uri = 'mongodb://127.0.0.1:27017/cryptoDB';
+const uri = 'mongodb+srv://90360saurabh:nuFAhbkz7eiMb2Se@cluster0.2pdrn0k.mongodb.net/cryptoDB';
 
 async function connectDB() {
     try {
